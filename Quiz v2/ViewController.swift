992a3d164fdb216f70cf10a9ffeb1a778de33c94
Jonathan Frohne <redaktion@redaktions-mac-mini.fritz.box>
2019-03-19 17:45:57 +0100

    Initial Commit

---
+++ Quiz v2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz v2
+//
+//  Created by Redaktion on 19.03.19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Quiz v2Tests/Quiz_v2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Quiz_v2Tests.swift
+//  Quiz v2Tests
+//
+//  Created by Redaktion on 19.03.19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import XCTest
+@testable import Quiz_v2
+
+class Quiz_v2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

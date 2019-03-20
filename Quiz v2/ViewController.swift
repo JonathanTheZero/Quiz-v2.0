517dd9967e7212f35a8cca0d58c261598fa256ea
Jonathan Frohne <redaktion@Redaktions-Mini.fritz.box>
2019-03-20 21:11:04 +0100

    Updated Syntax to Swift 4

--- Quiz v2/ViewController.swift
+++ Quiz v2/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController {
 
     //MARK: Actions
 
-    @IBAction func tapOnSettings(sender: UIButton) {
+    @IBAction func tapOnSettings(_ sender: UIButton) {
         headline.text = "Erfolgreich geklickt"
     }
     

3a02f177c449f2a9c2dbce6b205097371603b937
JonathanTheZero <jonathan.frohne@web.de>
2019-03-21 21:41:04 +0100

    Continued working on Leaderboard for the Highscores

---
+++ Quiz v2/QuizHighscoresTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuizHighscoresTableViewCell.swift
+//  Quiz v2
+//
+//  Created by Redaktion on 21.03.19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class QuizHighscoresTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

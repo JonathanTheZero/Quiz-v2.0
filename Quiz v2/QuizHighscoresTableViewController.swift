79a54b2a8cca54b5a720deaa7dd9f5399a3980ce
JonathanTheZero <jonathan.frohne@web.de>
2019-03-22 19:32:49 +0100

    -

--- Quiz v2/QuizHighscoresTableViewController.swift
+++ Quiz v2/QuizHighscoresTableViewController.swift
@@ -74,7 +74,7 @@ class QuizHighscoresTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source 
+            // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view

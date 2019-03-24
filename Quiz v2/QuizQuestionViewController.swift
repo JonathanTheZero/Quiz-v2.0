f691782ac2d12bd91cfd1e59faec5a94d0845490
JonathanTheZero <jonathan.frohne@web.de>
2019-03-24 17:49:02 +0100

    Started working on Quiz Interface

---
+++ Quiz v2/QuizQuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Quiz v2
+//
+//  Crated by Redaktion on 19.03.19.
+//  Copyright © 2019 Jonathan. All rights reserved.
+//
+
+import UIKit
+
+class QuizQuestionViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Actions
+    
+    func loadUI(){
+        
+    }
+    
+    
+    
+    
+}
+
